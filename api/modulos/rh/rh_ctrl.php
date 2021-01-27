d77f00e528b7d246ee64407dd2e6230bef4c9d37
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-01-27 11:08:58 -0600

    Se inicia crud clientes RMX

---
+++ api/modulos/rh/rh_ctrl.php
@@ -0,0 +1,20 @@
+<?php
+    switch($recurso){
+
+        case 'clientes':
+            $v = include_once(RH['clientes']);
+
+            if($v == TRUE){
+                $l = clientes::recurso($peticion);
+                $l['status']?$vista->est=200:$vista->est=401;
+                $vista->imprimir($l);
+            }else{
+                $vista->estado = 400;
+                $cuerpo = RECURSO_NO_IMPLEMENTADO;
+            }
+            break;
+    default:
+    $vista->estado = 400;
+    $cuerpo = RECURSO_NO_EXISTE;
+}
+?>
