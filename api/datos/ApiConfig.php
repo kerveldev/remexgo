60d6a74960f19651d4ca9b2c4decbbc36dab14de
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-01-29 11:29:46 -0600

    Se realiza modificacion al modulo de clientes

--- api/datos/ApiConfig.php
+++ api/datos/ApiConfig.php
@@ -199,7 +199,7 @@ define('RH',[
           'ruta'=>ROOT.'modulos/mmx/',
           'ruta_web'=> ROOT_WEB.'modulos/mmx/',
           'controlador'=>MODULOS.'rh/rh_ctrl.php',
-          'clientes'=>MOCULOS.'rh/clientes/Clientes'
+          'clientes'=>MOCULOS.'rh/clientes/Clientes.php'
      ]);
 
 define('QR',[
