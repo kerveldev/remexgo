ef70a9394fbda3870cf1cffb16c8677395a27e06
ChrystianRedin <36172834+ChrystianRedin@users.noreply.github.com>
2021-02-03 19:22:23 -0600

    Update menu.php

--- modulos/Secciones/menu.php
+++ modulos/Secciones/menu.php
@@ -24,7 +24,7 @@
                 REMEX
             </div>
         </li>
-        <li class="active">
+        <li>
             <a href="index.html"><i class="fa fa-pie-chart"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
             <ul class="nav nav-second-level">
        
