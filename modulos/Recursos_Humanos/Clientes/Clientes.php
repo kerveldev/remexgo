d57c41229c0058c6f3a43ff2d5a2111c5a6538c4
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-05 14:00:09 -0600

    mod php clientes

--- modulos/Recursos_Humanos/Clientes/Clientes.php
+++ modulos/Recursos_Humanos/Clientes/Clientes.php
@@ -39,156 +39,44 @@ $us = $_SESSION['user']['data'];
 
 <body>
      
-    <div class="modal dark_bg" id="modal_usuario" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="titulo" style="display: none;" aria-hidden="true">
-        <div class="modal-dialog modal-md" role="document">
+   
+
+    <div class="modal dark_bg" id="modal_usuario" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="titulo" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="titulo">Respaldo Documental</h5>
+                    <button type="button" class="close" onclick="cerrarModalImgAsignacion();" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+                </div>
+                  <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-16 col-sm-16">
+                                <!-- Inicio del Card -->
+                                <div class="card">
+                                    <!-- Card Body -->
+                                    <div class="card-body"> 
 
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="titulo">Datos del Usuario: <span id="nUsuario"></span></h5>
-                        <button type="button" class="close" onclick="cerrarModalUsuario_Id();" aria-label="Close"> <span aria-hidden="true">×</span> </button>
-                    </div>
-
-                    <div class="modal-body">
-                        <div class="card">
-
-                          <div class="card-body">
-
-                                <!-- <div class="ibox float-e-margins"> -->
-
-                                      <div id="wizard">
-                                          <h1>Personales</h1>
-                                          <div class="step-content">
-                                              <div class="text-center m-t-md">
-                                                
-                                                    <div class="row">
-
-                                                          <div class="col-sm-4">
-                                                            <label for="A_Paterno_usuario">A. Paterno:</label>
-                                                            <input type="text" class="form-control" name="A_Paterno_usuario" id="A_Paterno_usuario"> 
-                                                          </div>
-                      
-                                                          <div class="col-sm-4">
-                                                            <label for="A_Materno_usuario">A. Materno:</label>
-                                                            <input type="text" class="form-control" name="A_Materno_usuario" id="A_Materno_usuario"> 
-                                                          </div>
-                      
-                                                          <div class="col-sm-4">
-                                                            <label  for="Nombre_usuario">Nombre:</label>
-                                                            <input type="text" class="form-control" name="Nombre_usuario" id="Nombre_usuario"> 
-                                                          </div>  
-                        
-                                                      </div>
-
-                                                      <div class="row">
-
-                                                            <div class="col-sm-4">
-                                                              <label for="A_Paterno_usuario">Fecha Nacimiento:</label>
-                                                              <input type="date" class="form-control" name="A_Paterno_usuario" id="A_Paterno_usuario"> 
-                                                            </div>
-                        
-                                                            <div class="col-sm-4">
-                                                              <label for="A_Materno_usuario">Edad:</label>
-                                                              <input type="text" class="form-control" name="A_Materno_usuario" id="A_Materno_usuario"> 
-                                                            </div>
-                        
-                                                            <div class="col-sm-4">
-                                                              <label  for="Nombre_usuario">Nacionalidad:</label>
-                                                              <input type="text" class="form-control" name="Nombre_usuario" id="Nombre_usuario"> 
-                                                            </div>  
+                                       
                           
-                                                      </div>
-
-                                                      <div class="row">
-
-                                                            <div class="col-sm-4">
-                                                              <label for="entidad_nac_usuario">Entidad Nacimiento:</label>
-                                                              <input type="text" class="form-control" name="entidad_nac_usuario" id="entidad_nac_usuario"> 
-                                                            </div>
-                        
-                                                            <div class="col-sm-4">
-                                                              <label for="municipio_nac_usuario">Municipio Nacimiento:</label>
-                                                              <input type="text" class="form-control" name="municipio_nac_usuario" id="municipio_nac_usuario"> 
-                                                            </div>
-                        
-                                                            <div class="col-sm-4">
-                                                              <label  for="genero_usuario">Genero:</label>
-                                                              <input type="text" class="form-control" name="genero_usuario" id="genero_usuario"> 
-                                                            </div>  
-                          
-                                                    </div>
-
-                                                    <div class="row">
-
-                                                          <div class="col-sm-4">
-                                                            <label for="tipo_sangre_usuario">Tipo Sangre:</label>
-                                                            <input type="text" class="form-control" name="tipo_sangre_usuario" id="tipo_sangre_usuario"> 
-                                                          </div>
-                      
-                                                          <div class="col-sm-4">
-                                                            <label for="edo_civil_usuario">Estado Civil:</label>
-                                                            <input type="text" class="form-control" name="edo_civil_usuario" id="edo_civil_usuario"> 
-                                                          </div>
-                      
-                                                          <div class="col-sm-4">
-                                                            <label  for="e_mail_usuario">Correo Electronico:</label>
-                                                            <input type="text" class="form-control" name="e_mail_usuario" id="e_mail_usuario"> 
-                                                          </div>  
-                        
-                                                    </div>
-
-                                                    <div class="row">
-
-                                                          <div class="col-sm-4">
-                                                            <label for="telefono_usuario">Telefono:</label>
-                                                            <input type="text" class="form-control" name="telefono_usuario" id="telefono_usuario"> 
-                                                          </div>
-                      
-                                                          <div class="col-sm-4">
-                                                            <label for="cel_usuario">Celular:</label>
-                                                            <input type="text" class="form-control" name="cel_usuario" id="cel_usuario"> 
-                                                          </div>
-                      
-                                                          <div class="col-sm-4">
-                                                            <label  for="tel_2_usuario">Otro Telefono:</label>
-                                                            <input type="text" class="form-control" name="tel_2_usuario" id="tel_2_usuario"> 
-                                                          </div>  
-                        
-                                                  </div>
-              
-                                              </div>
+                                           <div class="form-row">
+                                            <div class="form-group col-sm-16 col-md-16 col-xl-16">
+                                                <form>
+                                                  <div class="file-loading">
+                                                      <input id="input-1" name="input-1" type="file" multiple="">
+                                                 </div>
+                                                </form>
+                                            </div> 
                                           </div>
-              
-                                          <h1>Second Step</h1>
-                                          <div class="step-content">
-                                              <div class="text-center m-t-md">
-                                                  
-                                                
-                                              </div>
-                                          </div>
-              
-                                          <h1>Third Step</h1>
-                                          <div class="step-content">
-                                              <div class="text-center m-t-md">
-                                                  <h2>This is step 3</h2>
-                                                  <p>
-                                                      This is last content.
-                                                  </p>
-                                              </div>
-                                          </div>
-                                      </div>
-                              <!-- </div> -->
-
-                          </div>
-
+                                      
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                  </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" onclick="cerrarModalImgAsignacion();">Cerrar</button>
 
-                    </div>
-                    
-                  <!-- <div class="modal-footer ">
-                      <button type="button " class="btn btn-secondary " onclick="cerrarModalUsuario_Id();" >Cerrar</button>
-                      <button type="button" class="btn btn-primary" onclick="guardarcambio();">Cambiar Contraseña</button>
-                  </div> -->
-            
+                </div>
             </div>
         </div>
     </div>
@@ -203,48 +91,6 @@ $us = $_SESSION['user']['data'];
                         </div>
                     </div>
 
-                      <!--<div class="row">
-                        <div class="col-sm-12">
-                          <div class="card">
-                            <div class="card-block">
-                            <div class="card-header">
-
-                              <h6 class="card-title">Listado Elementos</h6>
-                            </div>
-                            <div class="card-body">
-                              <div class="row">
-                                <div class="col-md-12">
-                                  <button class="btn btn-success " id="btn_agregar" onclick="abrirUsuario('')">Agregar Usuario</button> 
-                                </div>
-                              </div>
-                              <div class="row">
-                                <div class="col-md-12">
-                                  <table class="table table-striped nowrap" id="tabla_clientes">
-                                    <thead>
-                                      <th>Id</th>
-                                      <th>Cliente</th>
-                                      <th>Entidad</th>
-                                      <th>Status</th>
-                                      <th>Acciones</th>
-                                    </thead>
-                                    <tbody>
-                    
-                                    </tbody>
-                                    <tfoot>
-                                      <th>Id</th>
-                                      <th>Cliente</th>
-                                      <th>Entidad</th>
-                                      <th>Status</th>
-                                      <th>Acciones</th>
-                                    </tfoot>
-                                  </table>
-                                </div>
-                              </div>
-                            </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>-->
                       <div class="row">
                         <div class="col-lg-12">
                             <div class="ibox">
