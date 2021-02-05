6951c0dd0879853ad8086532959e3e3abcdd3acf
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-05 14:31:29 -0600

    mod php clientes

--- modulos/Recursos_Humanos/Clientes/Clientes.php
+++ modulos/Recursos_Humanos/Clientes/Clientes.php
@@ -134,7 +134,12 @@ $us = $_SESSION['user']['data'];
 
                                                     <div class="row">
                                                             <button class="btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>Log in</strong></button>
-                                                            <label class=""> <div class="icheckbox_square-green checked" style="position: relative;"><input type="checkbox" class="i-checks" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> Remember me </label>
+                                                            <label class=""> 
+                                                            <div class="icheckbox_square-green checked" style="position: relative;">
+                                                              <input type="checkbox" class="i-checks" style="position: absolute; opacity: 0;">
+                                                              <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px;  border: 0px; opacity: 0;">
+                                                              </ins>
+                                                            </div> Remember me </label>
                                                         </div>
 
               
