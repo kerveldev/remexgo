73644cb32a9a7e97dd1b16252852287fa17bc54f
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-08 10:42:50 -0600

    comentario modifivcacion

--- modulos/Recursos_Humanos/Clientes/Clientes.php
+++ modulos/Recursos_Humanos/Clientes/Clientes.php
@@ -138,7 +138,7 @@ $us = $_SESSION['user']['data'];
 
                                                             <label class="checkbox-inline"> 
                                                                 <input type="checkbox" value="btn_status" id="btn_status" style="width: 100%; height: 100%;">
-                                                            Status
+                                                            Statuss
                                                             </label>
                                                             <!--<div class="icheckbox_square-green" style="position: relative;">
                                                                 <input type="checkbox" id="btn_status" name="btn_status" class="i-checks" style="position: absolute; opacity: 0;">
