827ba90bbc18ae5bc38bf590e557b4fad5b46e8f
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-08 10:06:11 -0600

    name status

--- modulos/Recursos_Humanos/Clientes/Clientes.php
+++ modulos/Recursos_Humanos/Clientes/Clientes.php
@@ -138,7 +138,7 @@ $us = $_SESSION['user']['data'];
 
                                                             <label class=""> 
                                                             <div class="icheckbox_square-green" style="position: relative;">
-                                                                <input type="checkbox" id="btn_status"  class="i-checks" style="position: absolute; opacity: 0;">
+                                                                <input type="checkbox" id="btn_status" name="btn_status" class="i-checks" style="position: absolute; opacity: 0;">
                                                                 <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">
                                                                 </ins>
                                                             </div> 
