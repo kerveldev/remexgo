2897e5f7d358a70674c722a29fab13648c4612ad
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-08 10:16:00 -0600

    mod boton status

--- modulos/Recursos_Humanos/Clientes/js/Clientes.js
+++ modulos/Recursos_Humanos/Clientes/js/Clientes.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
     $('.i-checks').iCheck({
         checkboxClass: 'icheckbox_square-green',
         radioClass: 'iradio_square-green',
-        $("#Estatus").val("1"),
     });
 
     
