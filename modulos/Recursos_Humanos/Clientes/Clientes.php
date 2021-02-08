4fd4dc849fd8c0b95b994bd58f694d8beba324a3
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-08 10:17:36 -0600

    mod status clientes

--- modulos/Recursos_Humanos/Clientes/Clientes.php
+++ modulos/Recursos_Humanos/Clientes/Clientes.php
@@ -136,13 +136,13 @@ $us = $_SESSION['user']['data'];
                                                         <div class="col-sm-2">  
                                                             <br> 
 
-                                                            <label class=""> 
-                                                            <div class="icheckbox_square-green" style="position: relative;">
+                                                            <input class="form-control" type="checkbox" id="btn_status" name="btn_status" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">
+                                                            <!--<div class="icheckbox_square-green" style="position: relative;">
                                                                 <input type="checkbox" id="btn_status" name="btn_status" class="i-checks" style="position: absolute; opacity: 0;">
                                                                 <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">
                                                                 </ins>
                                                             </div> 
-                                                            Remember me </label>
+                                                            Remember me </label>-->
                                                             
                                                         </div>  
                                                     </div>
