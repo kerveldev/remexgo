5d00f2d7c406b368a7c40b55d90edf4b3736ed34
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-11 09:53:22 -0600

    mod listado articulos

--- modulos/Recursos_Humanos/Ventas/js/Ventas.js
+++ modulos/Recursos_Humanos/Ventas/js/Ventas.js
@@ -562,7 +562,7 @@ $(document).ready(function() {
                                 "<'row'<'col-sm-12'tr>>" +
                                 "<'row'<'#divisor.col-md-12'>>" +
                                 "<'row'<'col-sm-4 'l><'col-sm-4 '><'col-sm-4 floatRight'p>>" +
-                                "<'row'<'#divisor2.col-md-12'>>" +
+                                "<'row'<'#divisor2.col-md-12'>>",
                             columnDefs: [{
                                     'className': 'control',
                                 },
