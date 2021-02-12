0a57a35cd19b035addc5c54c5fe5abbe635420d9
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-12 14:26:43 -0600

    mod js ventas

--- modulos/Recursos_Humanos/Ventas/js/Ventas.js
+++ modulos/Recursos_Humanos/Ventas/js/Ventas.js
@@ -116,7 +116,8 @@ $(document).ready(function() {
                     "<input type='number' class='cantidad"+ Id[i]+"' max='"+Cantidad[i]+"'>",
                     "<input type='number'  step='0.01' class='descuento"+ Id[i]+"' value='"+Descuento[i]+"'>",
                     "$ "+Precio[i],
-                    "<input type='text' class='total"+"$ "+ Id[i]+"' readonly='readonly'>",
+                    //"<input type='text' class='total"+"$ "+ Id[i]+"'>",
+                    "<a type='text' class='total"+ Id[i]+"'></a>",
                     "<button type='button' class='btn btn-sm btn-outline btn-info p-2' onclick='total_articulo(\"" + Id[i] + "\",\"" + Precio[i] + "\")'; title='Informacion del cliente'><i class='fa fa-refresh'></i></button>&nbsp;",
                 ] ).draw( false );
                 
