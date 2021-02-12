036a15aaeb13ac4b83634f1f59b04cc9dca68b29
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-12 07:42:45 -0600

    MOD DATA TABLE VENTAS

--- modulos/Recursos_Humanos/Ventas/js/Ventas.js
+++ modulos/Recursos_Humanos/Ventas/js/Ventas.js
@@ -101,40 +101,7 @@ $(document).ready(function() {
 
             InicializarDatatable("tabla_clientes");
         }else{
-            var t = $('#tabla_clientes').DataTable({
-                "footerCallback": function ( row, data, start, end, display ) {
-                    var api = this.api(), data;
-         
-                    // Remove the formatting to get integer data for summation
-                    var intVal = function ( i ) {
-                        return typeof i === 'string' ?
-                            i.replace(/[\$,]/g, '')*1 :
-                            typeof i === 'number' ?
-                                i : 0;
-                    };
-         
-                    // Total over all pages
-                    total = api
-                        .column( 4 )
-                        .data()
-                        .reduce( function (a, b) {
-                            return intVal(a) + intVal(b);
-                        }, 0 );
-         
-                    // Total over this page
-                    pageTotal = api
-                        .column( 4, { page: 'current'} )
-                        .data()
-                        .reduce( function (a, b) {
-                            return intVal(a) + intVal(b);
-                        }, 0 );
-         
-                    // Update footer
-                    $( api.column( 4 ).footer() ).html(
-                        '$'+pageTotal +' ( $'+ total +' total)'
-                    );
-                }
-            });
+            var t = $('#tabla_clientes').DataTable();
             rep = Id.length;
             for(i=0;i<rep; i++){
                 t.row.add( [
