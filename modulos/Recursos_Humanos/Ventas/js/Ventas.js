4177929f22cec1bedee851b1bfaa90cec7e95ae0
Jacquelin <71983285+Jacquelin1889@users.noreply.github.com>
2021-02-12 06:49:54 -0600

    mod datatables

--- modulos/Recursos_Humanos/Ventas/js/Ventas.js
+++ modulos/Recursos_Humanos/Ventas/js/Ventas.js
@@ -94,47 +94,50 @@ $(document).ready(function() {
     
     function listadoClientes(Id,Nombre,Cantidad,Descuento,Precio){
        // var tabla = "tabla_clientes";
-        var t = $('#tabla_clientes').DataTable({
-            "footerCallback": function ( row, data, start, end, display ) {
-                var api = this.api(), data;
-     
-                // Remove the formatting to get integer data for summation
-                var intVal = function ( i ) {
-                    return typeof i === 'string' ?
-                        i.replace(/[\$,]/g, '')*1 :
-                        typeof i === 'number' ?
-                            i : 0;
-                };
-     
-                // Total over all pages
-                total = api
-                    .column( 4 )
-                    .data()
-                    .reduce( function (a, b) {
-                        return intVal(a) + intVal(b);
-                    }, 0 );
-     
-                // Total over this page
-                pageTotal = api
-                    .column( 4, { page: 'current'} )
-                    .data()
-                    .reduce( function (a, b) {
-                        return intVal(a) + intVal(b);
-                    }, 0 );
-     
-                // Update footer
-                $( api.column( 4 ).footer() ).html(
-                    '$'+pageTotal +' ( $'+ total +' total)'
-                );
-            }
-        });
+        
         cerrarModalArticulos();
         
         if(Id == "" || Id == undefined){
 
             InicializarDatatable("tabla_clientes");
         }else{
-        
+            var t = $('#tabla_clientes').DataTable({
+                paging:   false,
+                bFilter: false, 
+                bInfo: false,
+                "footerCallback": function ( row, data, start, end, display ) {
+                    var api = this.api(), data;
+         
+                    // Remove the formatting to get integer data for summation
+                    var intVal = function ( i ) {
+                        return typeof i === 'string' ?
+                            i.replace(/[\$,]/g, '')*1 :
+                            typeof i === 'number' ?
+                                i : 0;
+                    };
+         
+                    // Total over all pages
+                    total = api
+                        .column( 4 )
+                        .data()
+                        .reduce( function (a, b) {
+                            return intVal(a) + intVal(b);
+                        }, 0 );
+         
+                    // Total over this page
+                    pageTotal = api
+                        .column( 4, { page: 'current'} )
+                        .data()
+                        .reduce( function (a, b) {
+                            return intVal(a) + intVal(b);
+                        }, 0 );
+         
+                    // Update footer
+                    $( api.column( 4 ).footer() ).html(
+                        '$'+pageTotal +' ( $'+ total +' total)'
+                    );
+                }
+            });
             rep = Id.length;
             for(i=0;i<rep; i++){
                 t.row.add( [
